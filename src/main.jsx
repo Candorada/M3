aa26aaed1edff35c9997fa71d3649ca7e731b492
Candorada <51519091+Candorada@users.noreply.github.com>
2024-09-19 14:18:18 +0200

    message

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,16 @@ let brsRoutr = createBrowserRouter([
     path: '/',
     element: <App />,
     errorElement: <h1>404 - Page Not Found</h1>,
-  }
-])
-let router2 = createBrowserRouter([
+  },
   {
     path: '/assfuck',
     element: <App />,
     errorElement: <></>,
+    
   }
 ])
 createRoot(document.getElementById('root')).render(
   <>
   <RouterProvider router = {brsRoutr} />
-  <RouterProvider router = {router2} />
   </>
   )
