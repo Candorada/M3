2c66952654ec213fe0cfd2dfa0df5934bcb9b9b2
Candorada <51519091+Candorada@users.noreply.github.com>
2024-09-19 08:40:22 +0200

    ya

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+let brsRoutr = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>404 - Page Not Found</h1>,
+  }
+])
+let router2 = createBrowserRouter([
+  {
+    path: '/assfuck',
+    element: <App />,
+    errorElement: <></>,
+  }
+])
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <>
+  <RouterProvider router = {brsRoutr} />
+  <RouterProvider router = {router2} />
+  </>
+  )
