8ace9dc9e7eb3cdec9bc43d7c0ce041d2486ae25
Candorada <51519091+Candorada@users.noreply.github.com>
2024-09-23 09:32:29 +0200

    ya

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import SideNav from "./SideNavBar";
+import Library from "./library";
+import "./App.css"
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <SideNav />
+      <Library />
     </>
   );
 }
---
+++ src/library.jsx
@@ -0,0 +1,12 @@
+import "./library.css";
+function Library() {
+    return (<>
+        <div id="library">
+            <h1>Library</h1>
+        </div>
+    </>)
+}
+
+
+
+export default Library;
