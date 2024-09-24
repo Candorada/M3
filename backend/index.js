77162faafde951a766c7ec4db5406c26b8e39d40
Jesso3 <jesse2.olbrechts@gmail.com>
2024-09-24 12:22:52 +0200

    backend stuff
    yes

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,24 @@ let express = require("express");
 const app = express();
 const fileSystem = require("fs");
 let filenames = fileSystem.readdirSync(__dirname);
-names = [];
+let names = [];
 filenames.forEach((file) => {
   names.push(file);
 });
 
 app.get("/", (req, res) => {
-  res.send({ test: names });
+  res.json({ test: names });
 });
 
 app.get("/extensionList", (req, res) => {
   res.send(fileSystem.readdirSync("./extensions"));
 });
+
+app.get("/library", (req, res) => {
+  res.json({ categories: ["hi", "balls"], balls: "bye" });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}\nhttp://localhost:${port}`);
 });
-
