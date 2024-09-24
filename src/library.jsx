6eb3db72a224bb61db2c66befe2f9b6b7de4dd97
Candorada <51519091+Candorada@users.noreply.github.com>
2024-09-24 12:20:15 +0200

    ya

--- src/library.jsx
+++ src/library.jsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
 import "./library.css";
-let categories = ["manga","comics","movies","games","ebooks","audiobooks"]
+//let categories = ["manga","comics","movies","games","ebooks","audiobooks"]
 function Library() {
+    const [categories, setCat] = useState(["Default"]);
+    const catFetch = async () => {
+        let res = await fetch("http://localhost:3000/library");
+        console.log(res)
+        setCat((await res.json()).categories);
+    }
+      
+    useEffect(()=>{catFetch()},[])
+   
+
     return (<>
         <div id="library">
             <div id = "topbar">
