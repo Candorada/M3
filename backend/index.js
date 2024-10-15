a692e06b07c538073acc6853f6144825e01348d5
Jesso3 <jesse2.olbrechts@gmail.com>
2024-10-15 08:27:59 +0200

    some node mudole stuff

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const extensionPath = path.join(__dirname, "extensions");
 let filenames = fileSystem.readdirSync(extensionPath);
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 db.serialize(() => {
   db.run(
     `CREATE TABLE IF NOT EXISTS comics (
@@ -18,7 +18,7 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 name TEXT NOT NULL,
 source TEXT NOT NULL,
 upload_date TEXT NOT NULL,
-recent_access TEXT NOT NULL
+recent_acuess TEXT NOT NULL
 )`,
     (err) => {
       if (err) {
@@ -40,9 +40,9 @@ app.get("/:extension/search", (req, res) => {
   res.send(extension.search());
   //TODO: make compatible with multiple extensions later :)
 });
-app.post("/:extension/getInfo",async (req, res) => {
+app.post("/:extension/getInfo", async (req, res) => {
   const extension = require("./extensions/template/template.js");
-  const body = req.body
+  const body = req.body;
   res.json(await extension.getInfo(body.url));
   //TODO: make compatible with multiple extensions later :)
 });
@@ -85,12 +85,12 @@ app.get("/library", (req, res) => {
     balls: "bye",
   });
 });
-app.get("library/:category", (req,res)=>{
+app.get("library/:category", (req, res) => {
   res.json([
     //yes this is currently empty.
-  ])
+  ]);
   //TODO work later with multiple categories
-})
+});
 
 const port = 3000;
 app.listen(port, () => {
