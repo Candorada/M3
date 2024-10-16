926ba627410aa511dfdef6078632a351117acb1e
Jesso3 <jesse2.olbrechts@gmail.com>
2024-10-16 11:34:41 +0200

    started making the button to delete extensions, yes

--- src/extensionsUI/extenisonsUI.jsx
+++ src/extensionsUI/extenisonsUI.jsx
@@ -3,61 +3,91 @@ import "./style.css";
 
 function Extension({ extension }) {
   return (
-    <a href={"extensions/"+extension.id}>
-    <div className="extension">
-      <img src={extension.img} alt="" />
-      <label>{extension.name}</label>
-    </div>
+    <a href={"extensions/" + extension.id}>
+      <div className="extension">
+        <img src={extension.img} alt="" />
+        <label>{extension.name}</label>
+      </div>
     </a>
   );
 }
 function ExtensionsPage() {
   const [extensions, setExtensions] = useState([]);
-  var selectedFile = null
-  function updateExtensionList(){
+  var selectedFile = null;
+  function updateExtensionList() {
     fetch("http://localhost:3000/extensionList")
       .then((response) => response.json())
       .then((data) => {
         // Update the state with the fetched extensions
         setExtensions(
-          Object.keys(data).map((id) => ({ //id stands for extension ID aka the folder name
-            id:id,
-            name:data[id].properties.name,
-            img:(data[id].properties.iconPath?(data[id].properties.iconPath.match(/^http/)?"":"../../backend/extensions/"+id+"/")+data[id].properties.iconPath:"./src/extensionsUI/Error.jpg") // dont think too hard about this. its confusing
+          Object.keys(data).map((id) => ({
+            //id stands for extension ID aka the folder name
+            id: id,
+            name: data[id].properties.name,
+            img: data[id].properties.iconPath
+              ? (data[id].properties.iconPath.match(/^http/)
+                  ? ""
+                  : "../../backend/extensions/" + id + "/") +
+                data[id].properties.iconPath
+              : "./src/extensionsUI/Error.jpg", // dont think too hard about this. its confusing
           })),
         );
       })
       .catch((error) => console.error("Error fetching array:", error));
   }
-  const downloadExtension= async(e)=>{
+  const downloadExtension = async (e) => {
     e.preventDefault();
-    selectedFile = e.dataTransfer.files[0]
+    selectedFile = e.dataTransfer.files[0];
     const formData = new FormData();
-    formData.append('file', selectedFile);
+    formData.append("file", selectedFile);
 
-    const response = await fetch('http://localhost:3000/downloadExtension', {
-      method: 'POST',
-      body: formData
-    }).then(updateExtensionList)
+    const response = await fetch("http://localhost:3000/downloadExtension", {
+      method: "POST",
+      body: formData,
+    }).then(updateExtensionList);
     if (response.ok) {
-      console.log('File uploaded successfully');
+      console.log("File uploaded successfully");
     } else {
-      console.log('Error uploading file');
+      console.log("Error uploading file");
     }
-  }
+  };
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/button-press", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: "Button pressed" }),
+      });
+
+      const data = await response.json();
+      console.log("Response from server:", data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   useEffect(() => {
-    updateExtensionList()
-  },[]);
+    updateExtensionList();
+  }, []);
   return (
     <>
-    <div id = "extensionListHolder" onDrop={downloadExtension} onDragOver={(e)=>e.preventDefault()}>
-      {extensions.map((extension) => (
+      <div
+        id="extensionListHolder"
+        onDrop={downloadExtension}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        {extensions.map((extension) => (
           <Extension key={extension.id} extension={extension} />
         ))}
-    </div>
+        <button className="deleteButton" onClick={handleClick}>
+          delete
+        </button>
+      </div>
     </>
   );
 }
 
 export default ExtensionsPage;
-
