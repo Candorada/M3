a4367351ab4be391465694857497891944ad16ee
Candorada <51519091+Candorada@users.noreply.github.com>
2024-11-20 09:14:54 +0100

    aded image loading

--- src/chapterPage.jsx
+++ src/chapterPage.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
 function ChapterPage(){
-    return <div>Chapter Page</div>
+    const {mediaID,chapterID} = useParams();
+    let [images,setImages] = useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:3000/library/comics/${mediaID}/getchapter?chapterID=${chapterID}`)
+        .then((r)=>r.json())
+        .then((r)=>setImages(r))
+    },[])
+    return <div>{images.map((img)=><img src = {img}></img>)}</div>
 }
 export default ChapterPage
