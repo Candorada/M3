f6a6f28e5b12d072785b96e03d62697e84fd4b07
Krognard <126901074+Krognard@users.noreply.github.com>
2024-11-30 15:11:20 +0100

    Working more

--- backend/extensions/mangaDex/index.js
+++ backend/extensions/mangaDex/index.js
@@ -26,21 +26,37 @@ const properties = {
   };
  */
 async function search(search,page) {
-  //test change
-  console.log("test")
-  api = await (await fetch("https:api.mangadex.org/manga?includes[]=cover_art")).json()
 
-  console.log(api)
-  console.log(api.data[0].id)
+  //manga itself url: `https://api.mangadex.org/manga?limit=10&offset=${page}&title=${search}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc`
+  
+  //cover art url: `https://api.mangadex.org/cover?limit=10&offset=${page}&manga%5B%5D=${mangaID}&order%5BlatestUploadedChapter%5D=desc`
+  console.log("test 1")
+
+  //set up defaults
+  if (page == null){
+    page = "1"
+  }
+
+  if (search == null){
+    search = "hunter"
+  }
+  console.log(page)
+  console.log(search)
+  console.log("test")
+
+  //everything works up to here
+  mangas = await (await fetch(`https://api.mangadex.org/manga?limit=10&offset=${page}&title=${search}&includedTagsMode=AND&excludedTagsMode=OR&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5BlatestUploadedChapter%5D=desc`)).json()
+  console.log(managas)
+
   return {
     media: [
-      {
+      { //have mulitiple of these objects (one per comic) contains information on comics being displayed on the one page
         img: "https://mangadex.org/covers/fa933825-c0cb-41f4-94e5-38c042810dab/44bdcbe2-0b71-4854-ac5d-6c24f327f89f.jpg",
         name: "https://mangadex.org/title/" + api.data[0].attributes.title.en,
         url: "https://mangadex.org/title/" + api.data[0].id,
       },
     ],
-    pageCount: 0,
+    pageCount: 0, //total number of pages that you can cycle through in the website
   };
 }
 function getInfo(url) {}
