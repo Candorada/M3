61e8553098d3fed06855017c4b44548df2b77bfb
Krognard <126901074+Krognard@users.noreply.github.com>
2025-01-15 17:30:48 +0100

    stuff, don't get excited

--- backend/extensions/mangaDex/index.js
+++ backend/extensions/mangaDex/index.js
@@ -12,8 +12,8 @@ const properties = {
     <p>MangaDex is an online manga reader that caters to all languages. MangaDex is made by scanlators, for scanlators, and gives active scanlation groups complete control over their releases. All of the content on our site is uploaded either by users, scanlation groups, and in some instances the publishers themselves.</p>
     <p>MangaDex was created in January 2018 by the former admin and sole developer, Hologfx. Since then, MangaDex has been steadily growing, approaching 14 million unique visitors per month. The site is currently ran by 21+ unpaid volunteers.</p>
     <a title="https://discord.gg/vkW47gN5TY" href="https://discord.gg/vkW47gN5TY" target="_blank" rel="noopener nofollow noreferrer">The fastest way to contact us is on our Discord server.</a>`,
-  creator: "Krognard",
-  creatorSocials: "https://github.com/Krognard", //optional
+  creator: "anonymous", //optional
+  creatorSocials: "https://www.google.com/", //optional
 };
 
 async function search(search,page) {
