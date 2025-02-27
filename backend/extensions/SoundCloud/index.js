a3e50bd4a6fb2f361200471022b1bfbf5456b70b
Jesso3 <jesse2.olbrechts@gmail.com>
2025-02-27 11:43:58 +0100

    setup soundcloud extension

--- backend/extensions/SoundCloud/index.js
+++ backend/extensions/SoundCloud/index.js
@@ -9,35 +9,34 @@ const properties = {
     <p>MangaDex is an online manga reader that caters to all languages. MangaDex is made by scanlators, for scanlators, and gives active scanlation groups complete control over their releases. All of the content on our site is uploaded either by users, scanlation groups, and in some instances the publishers themselves.</p>
     <p>MangaDex was created in January 2018 by the former admin and sole developer, Hologfx. Since then, MangaDex has been steadily growing, approaching 14 million unique visitors per month. The site is currently ran by 21+ unpaid volunteers.</p>
     <a title="https://discord.gg/vkW47gN5TY" href="https://discord.gg/vkW47gN5TY" target="_blank" rel="noopener nofollow noreferrer">The fastest way to contact us is on our Discord server.</a>`,
-  creator: "Jesso3",
-  creatorSocials: "https://github.com/Jesso3", //optional
+  creator: "Finnegan hopefully",
+  creatorSocials: "https://github.com/krognard", //optional
   customItemPage: "itemPage.jsx",
 };
 
+function ifError(cb, el) {
+  var retVal = el;
+  try {
+    retVal = cb();
+  } catch {
+    return el;
+  }
+  return retVal;
+}
 
+function schema() {
+  let custom_sc = {
+    createColumns: `
+      id TEXT PRIMARY KEY,
+      title TEXT,
+      source TEXT,
+          `,
+    insertColumns: ["id", "title", "source"],
+    getValues: (data) => [data.id, data.title, data.source],
+  };
 
-
-
-/*
- 
-  await(await fetch('http://localhost:3000/SoundCloud/addToLibrary', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({url:"https://soundcloud.com/julian-orzabal-416027090/twist-and-shout",
-id: "SoundCloud-Twist-and-shout",
-title: "Twist-and-shout",
-artist: "Beatles",
-coverImage: "https://i1.sndcdn.com/artworks-5yojzEO8Gp0ZmTNz-xLjvTg-t500x500.jpg",
-length: "243",
-tags: "good"}),
-})).json()
-
-
- */
-
-
+  return custom_sc;
+}
 async function search(search, page) {}
 
 async function getInfo(url) {}
