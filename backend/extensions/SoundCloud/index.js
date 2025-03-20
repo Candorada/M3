5c4e2ca7eb2f85bada500bf0d0bb70284a8b5275
Krognard <126901074+Krognard@users.noreply.github.com>
2025-03-20 20:49:36 +0100

    post computer apocolypse

--- backend/extensions/SoundCloud/index.js
+++ backend/extensions/SoundCloud/index.js
@@ -1,4 +1,5 @@
 var { parse } = require("node-html-parser");
+// var { parser } = require("./node_modules/node-html-parser");
 const properties = {
   name: "SoundCloud",
   type: "Music",
@@ -10,10 +11,69 @@ const properties = {
   creatorSocials: "https://github.com/krognard", //optional
   customItemPage: "itemPage.jsx",
 };
+// var { parser } = require("./node_modules/node-html-parser");
 
-async function search(search, page) {}
+async function search(search, page) {
 
-async function getInfo(url) {}
+  const result = await (
+    await fetch(
+      search
+        ? `https://soundcloud.com/search?q=call%20me%20maybe`
+        : `https://soundcloud.com/search?q=${search}`,
+    )
+  ).text();
+
+  let root = parse(result);
+  let body = root.querySelector("body");
+  let stuff2 = body.querySelectorAll(
+    ".sc-artwork.sc-artwork-20x.sc-artwork-placeholder-3.image__full.g-opacity-transition"
+  );
+  console.log(stuff2);
+
+  console.log(stuff2[0].style["background-image"]); // this line isn't doing anything
+
+
+
+
+
+
+  
+  let dataInner = {
+  img: `https://images.squarespace-cdn.com/content/v1/5e10bdc20efb8f0d169f85f9/09943d85-b8c7-4d64-af31-1a27d1b76698/arrow.png`,
+  name: "test",
+  url: "https://mangadex.org",
+  };
+
+  let data = []
+  for(let i = 0; i < 10; i++){
+    data.push(dataInner);
+  }
+  return await {
+    media: data,
+    pageCount: 5
+}
+
+
+}
+
+async function getInfo(url) {
+  const SoundCloud = require("soundcloud-scraper");
+  const client = new SoundCloud.Client();
+  const fs = require("fs");
+
+client.getSongInfo(url)
+    .then(async song => {
+        const stream = await song.downloadProgressive();
+        const writer = stream.pipe(fs.createWriteStream(`./${song.title}.mp3`));
+        writer.on("finish", () => {
+          console.log("Finished writing song!")
+          process.exit(1);
+        });
+    })
+    .catch(console.error);
+
+
+}
 
 module.exports = {
   search: search,
